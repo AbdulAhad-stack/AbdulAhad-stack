72388049fe1c762ad9596abbf2e2ada8037acc25
AbdulAhad-stack <ab60789170@gmail.com>
2025-04-10 11:46:02 +0500

    Add calculator.html using choose option

---
+++ simple calculator/javascript.js
@@ -0,0 +1,34 @@
+// var a;
+// var b;
+// var p;
+// var q;
+// var r;
+// var s;
+// var operation=0;
+// alert("Enter two numbers to perform operation");
+// a=prompt("Enter first number");
+// b=prompt("Enter second number");
+// alert("Enter the operation you want to perform like add, subtract,multiplication,division");
+// operation=prompt("enter your operation");
+// if(operation==="add"){
+//   p=a+b;
+//   alert(p);
+// }
+
+//  else if(operation==="subtract"){
+//   q=a-b;
+//   alert(q);
+// }
+
+// else if(operation==="multiplication"){
+//   r=a*b;
+//   alert(r);
+// }
+
+// else if(operation==="division"){
+//   s=a/b;
+//   alert(s);
+// }
+// else{
+//   alert("you entered an invalid number");
+// }
